b769499ee5585463a598c53fe4bbf57f5bafb228
maianh <nguyenanh09.dfl@gmail.com>
2023-12-19 10:21:54 +0700

    connect db

---
+++ src/main/java/com/PoilerPlate.java
@@ -0,0 +1,10 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class PoilerPlate {
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingDataMysqlApplication.class, args);
+    }
+}
